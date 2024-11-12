205badb403065cb6b5af88e455ac543168e59f89
kcarrillo <kcarrillo@griddynamics.com>
2024-11-12 15:11:39 -0600

    Problem 26 -> Solved

---
+++ Problem_26/src/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int ptr = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != nums[ptr])
+                nums[++ptr] = nums[i];
+        }
+
+        return ptr + 1;
+    }
+}
