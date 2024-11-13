06e7f0c3a4142eb3cb9f11e59d75048162dd56a6
kcarrillo <kcarrillo@griddynamics.com>
2024-11-13 12:25:21 -0600

    Problem 33 -> Solved

---
+++ Problem_33/src/Solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int search(int[] nums, int target) {
+        int first = 0, last = nums.length - 1;
+
+        while (first <= last) {
+            int mid = (first + last) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            } else if (nums[first] <= nums[mid]) {
+                if (target >= nums[first] && target < nums[mid]) {
+                    last = mid - 1;
+                } else {
+                    first = mid + 1;
+                }
+            } else {
+                if (target <= nums[last] && target > nums[mid]) {
+                    first = mid + 1;
+                } else {
+                    last = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
